6f4440720703715250c6882cc28e9f7525fa694f
Jerry <wzl-pc@wangzhilideMacBook-Pro.local>
2017-02-15 23:34:55 +0800

    add bugly

---
+++ app/src/main/java/com/vince/dbtest/Activities/UpgradeActivity.java
@@ -0,0 +1,15 @@
+package com.vince.dbtest.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vince.dbtest.R;
+
+public class UpgradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrade);
+    }
+}
