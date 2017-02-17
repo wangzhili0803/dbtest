6a36206144768a46cb670b5c48c16f82d0802e64
Jerry <wzl-pc@wangzhilideMacBook-Pro.local>
2017-02-17 15:35:47 +0800

    update tinker

--- app/src/main/java/com/jerry/dbtest/Activities/MainStartActivity.java
+++ app/src/main/java/com/jerry/dbtest/Activities/MainStartActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.tencent.bugly.beta.Beta;
-import com.tencent.bugly.beta.UpgradeInfo;
 import com.jerry.dbtest.R;
 
 public class MainStartActivity extends AppCompatActivity {
@@ -27,9 +25,8 @@ public class MainStartActivity extends AppCompatActivity {
         checkUpgradeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                /***** 检查更新 *****/
-                Beta.checkUpgrade();
+//                /***** 检查更新 *****/
+//                Beta.checkUpgrade();
             }
         });
 
@@ -43,33 +40,33 @@ public class MainStartActivity extends AppCompatActivity {
     }
 
     private void loadUpgradeInfo() {
-        if (upgradeInfoTv == null)
-            return;
-
-        /***** 获取升级信息 *****/
-        UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
-
-        if (upgradeInfo == null) {
-            upgradeInfoTv.setText("无升级信息");
-            return;
-        }
-        StringBuilder info = new StringBuilder();
-        info.append("id: ").append(upgradeInfo.id).append("\n");
-        info.append("标题: ").append(upgradeInfo.title).append("\n");
-        info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n");
-        info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n");
-        info.append("versionName: ").append(upgradeInfo.versionName).append("\n");
-        info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n");
-        info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n");
-        info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n");
-        info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n");
-        info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n");
-        info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n");
-        info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n");
-        info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType).append("\n");
-        info.append("图片地址：").append(upgradeInfo.imageUrl);
-
-
-        upgradeInfoTv.setText(info);
+//        if (upgradeInfoTv == null)
+//            return;
+//
+//        /***** 获取升级信息 *****/
+//        UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
+//
+//        if (upgradeInfo == null) {
+//            upgradeInfoTv.setText("无升级信息");
+//            return;
+//        }
+//        StringBuilder info = new StringBuilder();
+//        info.append("id: ").append(upgradeInfo.id).append("\n");
+//        info.append("标题: ").append(upgradeInfo.title).append("\n");
+//        info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n");
+//        info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n");
+//        info.append("versionName: ").append(upgradeInfo.versionName).append("\n");
+//        info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n");
+//        info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n");
+//        info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n");
+//        info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n");
+//        info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n");
+//        info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n");
+//        info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n");
+//        info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType).append("\n");
+//        info.append("图片地址：").append(upgradeInfo.imageUrl);
+//
+//
+//        upgradeInfoTv.setText(info);
     }
 }
--- app/src/main/java/com/jerry/dbtest/SampleApplicationLike.java
+++ app/src/main/java/com/jerry/dbtest/SampleApplicationLike.java
@@ -4,37 +4,37 @@ import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.os.Build;
 import android.support.multidex.MultiDex;
 
-import com.tencent.bugly.Bugly;
-import com.tencent.bugly.beta.Beta;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by wzl-pc on 2017/2/16.
  */
-
+@SuppressWarnings("unused")
+@DefaultLifeCycle(application = "com.jerry.FYApplication",flags = ShareConstants.TINKER_ENABLE_ALL,loadVerifyFlag = false)
 public class SampleApplicationLike extends DefaultApplicationLike {
 
     public static final String TAG = "Tinker.SampleApplicationLike";
+    private Application application;
 
-    public SampleApplicationLike(Application application, int tinkerFlags,
-                                 boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime,
-                                 long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources,
-                                 ClassLoader[] classLoader, AssetManager[] assetManager) {
-        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
-                applicationStartMillisTime, tinkerResultIntent);
+    public SampleApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag,
+                                 long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag,applicationStartElapsedTime,applicationStartMillisTime,tinkerResultIntent);
     }
+
 
 
     @Override
     public void onCreate() {
         super.onCreate();
         // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
-        Bugly.init(getApplication(), "202d1fda06", true);
+//        Bugly.init(getApplication(), "202d1fda06", true);
+        application = getApplication();
     }
 
 
@@ -46,8 +46,8 @@ public class SampleApplicationLike extends DefaultApplicationLike {
         MultiDex.install(base);
 
         // 安装tinker
-        // TinkerManager.installTinker(this); 替换成下面Bugly提供的方法
-        Beta.installTinker(this);
+         TinkerInstaller.install(this); //替换成下面Bugly提供的方法
+//        Beta.installTinker(this);
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
