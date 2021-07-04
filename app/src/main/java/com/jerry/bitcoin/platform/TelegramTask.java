4b0f8f7f7016526611c85ed21b980e83290d6a75
jerry <1339211614@qq.com>
2021-07-04 14:26:43 +0800

    也知道不能没有骨气

--- app/src/main/java/com/jerry/bitcoin/platform/TelegramTask.java
+++ app/src/main/java/com/jerry/bitcoin/platform/TelegramTask.java
@@ -2,11 +2,15 @@ package com.jerry.bitcoin.platform;
 
 import java.util.List;
 
+import android.graphics.Rect;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
 
 import com.jerry.baselib.Key;
 import com.jerry.baselib.assibility.EndCallback;
 import com.jerry.baselib.common.bean.AVObjQuery;
+import com.jerry.baselib.common.util.AppUtils;
 import com.jerry.baselib.common.util.CollectionUtils;
 import com.jerry.baselib.common.util.ToastUtil;
 import com.jerry.baselib.common.util.UserManager;
@@ -23,12 +27,22 @@ import com.jerry.bitcoin.beans.ScriptWord;
 public class TelegramTask {
 
     private static final String PACKAGE_NAME = "org.telegram.messenger:id/";
+    protected static int ERRORCOUNT = 5;
+    protected int taskIndex;
+    protected int errorCount;
 
     private boolean isHomePage(final ListenerService service) {
         AccessibilityNodeInfo root = service.getRootInActiveWindow();
         List<AccessibilityNodeInfo> texts = root.findAccessibilityNodeInfosByText(Key.SPACE);
         AccessibilityNodeInfo flowBtnNode = texts.get(texts.size() - 1);
         return "Open navigation menu".contentEquals(flowBtnNode.getContentDescription());
+    }
+//service.getRootInActiveWindow().findFocus(1);
+    private boolean isSearchPage(final ListenerService service) {
+        AccessibilityNodeInfo root = service.getRootInActiveWindow();
+        AccessibilityNodeInfo search = root.findFocus(AccessibilityNodeInfoCompat.FOCUS_INPUT);
+        return search != null && search.getText() != null
+            && ("Search".contentEquals(search.getText().toString()) || "搜索".contentEquals(search.getText().toString()));
     }
 
     public void doTask(final ListenerService service) {
@@ -62,8 +76,76 @@ public class TelegramTask {
     }
 
     private void enterRoom(final ListenerService service, final String roomId, final EndCallback endCallback) {
-        if (isHomePage(service)) {
-
+        if (errorCount > ERRORCOUNT || !AppUtils.playing) {
+            errorCount = 0;
+            taskIndex = 0;
+            endCallback.onEnd(false);
+            return;
         }
+        AccessibilityNodeInfo root = service.getRootInActiveWindow();
+        int tempTaskIndex = taskIndex;
+        switch (taskIndex) {
+            case 0:
+                if (isSearchPage(service)) {
+                    taskIndex++;
+                    taskIndex++;
+                } else if (isHomePage(service)) {
+                    taskIndex++;
+                } else {
+                    service.back();
+                }
+                break;
+            case 1:
+                List<AccessibilityNodeInfo> texts = root.findAccessibilityNodeInfosByText(Key.SPACE);
+                AccessibilityNodeInfo flowBtnNode = texts.get(texts.size() - 1);
+                Rect rect = new Rect();
+                flowBtnNode.getBoundsInScreen(rect);
+                if (service.exeClick(ListenerService.mWidth - 10, (rect.top + rect.bottom) >> 1)) {
+                    taskIndex++;
+                }
+                break;
+            case 2:
+                AccessibilityNodeInfo search = root.findFocus(AccessibilityNodeInfoCompat.FOCUS_INPUT);
+                if (service.input(search, roomId)) {
+                    taskIndex++;
+                }
+                break;
+            case 3:
+                AccessibilityNodeInfo search1 = root.findFocus(AccessibilityNodeInfoCompat.FOCUS_INPUT);
+                search1.getParent().getParent().getParent().getParent().getParent().getParent().getChild(1) .getChild(0).getChild(0);
+                taskIndex++;
+                break;
+            case 4:
+                AccessibilityNodeInfo search2 = root.findFocus(AccessibilityNodeInfoCompat.FOCUS_INPUT);
+                List<AccessibilityNodeInfo> globalSearch = root.findAccessibilityNodeInfosByText("全局搜索");
+                if (CollectionUtils.isEmpty(globalSearch) || !globalSearch.get(0).isVisibleToUser()) {
+                    globalSearch = root.findAccessibilityNodeInfosByText("global search");
+                }
+                Rect rect1 = new Rect();
+                search2.getParent().getBoundsInScreen(rect1);
+                if (CollectionUtils.isEmpty(globalSearch) || !globalSearch.get(0).isVisibleToUser()) {
+                    if (service.exeClick(ListenerService.mWidth >> 1, ((rect1.top + rect1.bottom) >> 1) + rect1.height())) {
+                        taskIndex++;
+                    }
+                } else {
+                    Rect rect2 = new Rect();
+                    globalSearch.get(0).getBoundsInScreen(rect2);
+                    if (service.exeClick(ListenerService.mWidth >> 1, ((rect2.top + rect2.bottom) >> 1) + rect1.height())) {
+                        taskIndex++;
+                    }
+                }
+                break;
+            case 5:
+                break;
+            default:
+                errorCount = 0;
+                taskIndex = 0;
+                endCallback.onEnd(true);
+                return;
+        }
+        if (taskIndex == tempTaskIndex) {
+            errorCount++;
+        }
+        service.postDelayed(() -> enterRoom(service, roomId, endCallback));
     }
 }
